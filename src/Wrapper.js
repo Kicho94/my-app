1c89e70e490b2a7ddb0f6f9f29cf3f7377d6f83e
Jovanoska <marija.jovanoska.maca@gmail.com>
2019-10-02 20:30:01 +0200

    axios

--- src/Wrapper.js
+++ src/Wrapper.js
@@ -1,14 +1,37 @@
 import React from 'react'
 import data from '../data'
 import User1 from './User1'
-
+import axios from 'axios'
 export default class Wrapper extends React.Component{
+        constructor(props){
+               super(props)
+               this.state = {
+                     users: []
+               }
+        }
+    componentDidMount () {
+       axios.get('https://jsonplaceholder.typicode.com/users')
+       .then((response) => {
+              const users = response.data.map(element => {
+             return <User1 key={element.id} name={element.name} email={element.email} />      
+              })
+              
+              this.setState({users: users})
+      
+       }) 
+       .catch((error) =>{
+              console.log(error)
+       }) 
+    }
+      
+    
     render(){
         
-      let users= data.map(element => {return <User1 key={element.id} name={element.name} email={element.email} /> })
+                                                                      //       let users= data.map(element => 
+                                                                      //        {return <User1 key={element.id} name={element.name} email={element.email} /> })
        return (
                <React.Fragment>   
-        {users}
+        {this.state.usersusers}
         {this.props.children}
         </React.Fragment>
                )
